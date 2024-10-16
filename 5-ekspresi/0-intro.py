af07887ba848a907fd4b4bc2163c711f41ee4bc4
Mohammad Amadeus Andika Fadhil <150133437+Mazdeus@users.noreply.github.com>
2024-10-16 12:54:08 +0700

    Add files via upload

---
+++ 5-ekspresi/0-intro.py
@@ -0,0 +1,22 @@
+# penggabungan dan replikasi pada list
+angka = [2, 4, 6, 8]
+huruf = ['P', 'Y', 'T', 'H', 'O', 'N']
+gabung = angka + huruf
+
+print(gabung)
+
+"""
+Output:
+[2, 4, 6, 8, 'P', 'Y', 'T', 'H', 'O', 'N']
+"""
+
+# menggunakan operator perkalian
+learn = ['P', 'Y', 'T', 'H', 'O', 'N']
+replikasi = learn * 2
+
+print(replikasi)
+
+"""
+Output:
+['P', 'Y', 'T', 'H', 'O', 'N', 'P', 'Y', 'T', 'H', 'O', 'N']
+"""
