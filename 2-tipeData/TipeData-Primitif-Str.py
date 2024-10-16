86a1cd7829ea4b6f86e89492a2d017c1c2d6f3ae
Mohammad Amadeus Andika Fadhil <150133437+Mazdeus@users.noreply.github.com>
2024-10-16 12:52:34 +0700

    Add files via upload

---
+++ 2-tipeData/TipeData-Primitif-Str.py
@@ -0,0 +1,47 @@
+multi_line = """Halo!
+Kapan terakhir kali kita bertemu?
+Kita bertemu hari Jum’at yang lalu."""
+
+print(multi_line)
+
+"""
+Output:
+Halo!
+Kapan terakhir kali kita bertemu?
+Kita bertemu hari Jum’at yang lalu.
+"""
+
+
+x = 'Dicoding'
+print(x[0])
+
+""" 
+Output:
+D
+"""
+
+"""
+x = 'Dicoding'
+x[0] = 'F'
+"""
+
+""" 
+Output:
+TypeError: 'str' object does not support item assignment
+"""
+
+name = "Perseus Evans"
+print("Hello, nama saya {name}")
+ 
+"""
+Output: 
+Hello, nama saya Perseus Evans
+"""
+
+name = "Perseus Evans"
+print("Nama saya %s" % (name))
+ 
+"""
+Output: 
+Nama saya Perseus Evans
+"""
