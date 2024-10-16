b820ab3cfa7c9d52a68ac07413882a94579806c1
Mohammad Amadeus Andika Fadhil <150133437+Mazdeus@users.noreply.github.com>
2024-10-16 12:54:28 +0700

    Add files via upload

---
+++ 6-aksiSekuensial/1-oneLiner.py
@@ -0,0 +1,37 @@
+# program menukar nilai pada dua buah variabel
+# x = 1
+# y = 2
+
+# temp = x
+# x = y
+# y = temp
+
+# print("Setelah pertukaran: ")
+# print("x = ", x)
+# print("y =",  y)
+
+# """
+# Output:
+# Setelah pertukaran: 
+# x = 2
+# y = 1
+# """
+
+# menggunakan konsep one liner
+x = 1
+y = 2
+
+x, y = y, x    # One-liner
+
+print('Setelah pertukaran: ')
+print('x =', x)
+print('y =', y)
+
+
+
+"""
+Output:
+Setelah pertukaran: 
+x = 2
+y = 1
+"""
