6e14e7a1aba6db662068aab4ef8209301f188855
Mazdeus <fadhilcr1@gmail.com>
2024-10-18 10:46:56 +0700

    styleGuide

---
+++ 12-styleGuide/3-statementGabungan/1-anotasiFungsi.py
@@ -0,0 +1,12 @@
+def luasPersegi_Panjang(panjang : int = 2, lebar : int = None) :
+    luas = panjang*lebar
+    return luas
+
+luas_satu = luasPersegi_Panjang(lebar=2)
+print(luas_satu)
+
+"""
+Output :
+4
+
+"""
