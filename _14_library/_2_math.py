a2789c6e056068714a1acbe337561e0b683e56ec
Mazdeus <fadhilcr1@gmail.com>
2025-01-13 16:08:42 +0700

    library

--- _14_library/_2_math.py
+++ _14_library/_2_math.py
@@ -1,5 +1,26 @@
 import math
 
+print("It's math! It has type {}".format(type(math)))
+
+print(dir(math))
+
+#============================================================
+
+"""Jika kita tahu bahwa kita akan sering menggunakan fungsi matematika, kita dapat 
+mengimpornya dengan nama alias yang lebih pendek untuk menghemat pengetikan 
+(meskipun dalam kasus ini “matematika” sudah cukup singkat).
+
+"""
+import math as mt
+mt.pi
+
+# Kita bisa merujuk ke semua variable yang ada di library math 
+from math import *
+"""
+import * membuat semua variabel modul dapat diakses secara 
+langsung oleh Anda (tanpa awalan titik-titik).
+"""
+
 print(math.sqrt(49))
 print(math.pi)
 
